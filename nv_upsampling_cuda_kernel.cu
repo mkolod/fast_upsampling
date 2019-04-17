4716182555103e79f214d6a3e8113aeb5ab60b92
Marek Kolodziej <mkolod@gmail.com>
2019-04-17 14:58:55 -0700

    Verified fprop

--- nv_upsampling_cuda_kernel.cu
+++ nv_upsampling_cuda_kernel.cu
@@ -7,61 +7,55 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-template <typename scalar_t, typename std::enable_if<std::is_same<c10::Half, scalar_t>::value>::type* = nullptr>
-__device__ __forceinline__
-void fastSpecializedAtomicAdd(scalar_t* tensor,
-                                  int index, int numel,
-                                  scalar_t value) {
+template <typename scalar_t, typename std::enable_if<std::is_same<
+                                 c10::Half, scalar_t>::value>::type * = nullptr>
+__device__ __forceinline__ void fastSpecializedAtomicAdd(scalar_t *tensor,
+                                                         int index, int numel,
+                                                         scalar_t value) {
 
+  if (index % 2 == 0 && index < (numel - 1)) {
 
-    if (index % 2 == 0 && index < (numel - 1)) {
+    __half2 value2;
+    value2.x = value;
+    value2.y = __int2half_rz(0);
+    atomicAdd(reinterpret_cast<__half2 *>(tensor) + index / 2, value2);
 
-      __half2 value2;
-      value2.x = value;
-      value2.y = __int2half_rz(0);
-      atomicAdd(reinterpret_cast<__half2*>(tensor) + index/2, value2);
+  } else if (index % 2 == 1) {
 
-    } else if (index % 2 == 1) {
+    __half2 value2;
+    value2.x = __int2half_rz(0);
+    value2.y = value;
+    atomicAdd(reinterpret_cast<__half2 *>(tensor) + index / 2, value2);
 
-      __half2 value2;
-      value2.x = __int2half_rz(0);
-      value2.y = value;
-      atomicAdd(reinterpret_cast<__half2*>(tensor) + index/2, value2);
+  } else {
 
-    } else {
-
-     atomicAdd(reinterpret_cast<__half*>(tensor) + index, static_cast<__half>(value));
-   }
+    atomicAdd(reinterpret_cast<__half *>(tensor) + index,
+              static_cast<__half>(value));
+  }
 }
 
-template <typename scalar_t, typename std::enable_if<!std::is_same<c10::Half, scalar_t>::value>::type* = nullptr>
-__device__ __forceinline__ void fastSpecializedAtomicAdd(scalar_t* tensor,
-                                  int index, int numel,
-                                  scalar_t value) {
+template <typename scalar_t, typename std::enable_if<!std::is_same<
+                                 c10::Half, scalar_t>::value>::type * = nullptr>
+__device__ __forceinline__ void fastSpecializedAtomicAdd(scalar_t *tensor,
+                                                         int index, int numel,
+                                                         scalar_t value) {
 
   atomicAdd(tensor + index, value);
-
 }
 
 template <class scalar_t>
-__device__  __forceinline__ void fastAtomicAdd(scalar_t* __restrict__ tensor,
-    int index, int numel, scalar_t value) {
+__device__ __forceinline__ void fastAtomicAdd(scalar_t *__restrict__ tensor,
+                                              int index, int numel,
+                                              scalar_t value) {
 
-
-    fastSpecializedAtomicAdd(tensor, index, numel, value);
-
+  fastSpecializedAtomicAdd(tensor, index, numel, value);
 }
 
 // https://github.com/pytorch/pytorch/blob/master/aten/src/THCUNN/TemporalUpSamplingLinear.cu
 
-__device__ __forceinline__ int idx(
-    const int n,
-    const int num_channels,
-    const int c,
-    const int height,
-    const int width,
-    const int y,
-    const int x) {
+__device__ __forceinline__ int idx(const int n, const int num_channels,
+                                   const int c, const int height,
+                                   const int width, const int y, const int x) {
   return ((n * num_channels + c) * height + y) * width + x;
 }
 
@@ -70,124 +64,93 @@ __device__ __forceinline__ int idx(
 // input is X, output is Y
 template <typename scalar_t>
 __global__ void bilinearForwardKernel(
-    const int output_size,
-    const int num_channels,
-    const int input_height,
-    const int input_width,
-    const int output_height,
-    const int output_width,
-    const scalar_t* const  __restrict__ X,
-    scalar_t* const __restrict__ Y) {
+    const int output_size, const int num_channels, const int input_height,
+    const int input_width, const int output_height, const int output_width,
+    const scalar_t *const __restrict__ X, scalar_t *const __restrict__ Y) {
 
-    const float height_scale = 1.0f * output_height / input_height;
-    const float width_scale = 1.0f * output_width / input_width;
+  const float height_scale = 1.0f * output_height / input_height;
+  const float width_scale = 1.0f * output_width / input_width;
 
-    const int batch_size = output_size / num_channels / output_height / output_width;
+  const int batch_size =
+      output_size / num_channels / output_height / output_width;
 
-    const int index = blockDim.x * blockIdx.x + threadIdx.x;
+  const int index = blockDim.x * blockIdx.x + threadIdx.x;
 
-//  for (size_t index = blockDim.x * blockIdx.x + threadIdx.x;
-//       index < output_size; index += blockDim.x * gridDim.x) {
+  //  for (size_t index = blockDim.x * blockIdx.x + threadIdx.x;
+  //       index < output_size; index += blockDim.x * gridDim.x) {
 
-    // TODO: loop over n and in within a thread
+  // TODO: loop over n and in within a thread
 
-    for (int n = 0; n < batch_size; n++) {
+  for (int n = 0; n < batch_size; n++) {
 
-     for (int c = 0; c < num_channels; c++) {
+    for (int c = 0; c < num_channels; c++) {
 
-    int indexTemp = index;
-    const int out_x = indexTemp % output_width;
-    indexTemp /= output_width;
-    const int out_y = indexTemp % output_height;
-//    indexTemp /= output_height;
+      int indexTemp = index;
+      const int out_x = indexTemp % output_width;
+      indexTemp /= output_width;
+      const int out_y = indexTemp % output_height;
+      //    indexTemp /= output_height;
 
-//    const int c = indexTemp % num_channels;
-//    indexTemp /= num_channels;
-//    const int n = indexTemp;
+      //    const int c = indexTemp % num_channels;
+      //    indexTemp /= num_channels;
+      //    const int n = indexTemp;
 
-    const int in_y = fminf(out_y / height_scale, input_height - 1);
-    const int in_x = fminf(out_x / width_scale, input_width - 1);
+      const int in_y = fminf(out_y / height_scale, input_height - 1);
+      const int in_x = fminf(out_x / width_scale, input_width - 1);
 
-    const float rheight =
-        output_height > 1 ? (input_height - 1.f) / (output_height - 1.f) : 0.f;
-    const float rwidth =
-        output_width > 1 ? (input_width - 1.f) / (output_width - 1.f) : 0.f;
+      const float rheight = output_height > 1
+                                ? (input_height - 1.f) / (output_height - 1.f)
+                                : 0.f;
+      const float rwidth =
+          output_width > 1 ? (input_width - 1.f) / (output_width - 1.f) : 0.f;
 
-    // Compute Y axis lambdas
-    const float h1r = rheight * out_y;
-    const int h1 = static_cast<int>(h1r);
-    const int h1p = (h1 < input_height - 1) ? 1 : 0;
-    const float h1lambda = h1r - h1;
-    const float h0lambda = 1.f - h1lambda;
+      // Compute Y axis lambdas
+      const float h1r = rheight * out_y;
+      const int h1 = static_cast<int>(h1r);
+      const int h1p = (h1 < input_height - 1) ? 1 : 0;
+      const float h1lambda = h1r - h1;
+      const float h0lambda = 1.f - h1lambda;
 
-    // Compute X axis lambdas
-    const float w1r = rwidth * out_x;
-    const int w1 = static_cast<int>(w1r);
-    const int w1p = (w1 < input_width - 1) ? 1 : 0;
-    const float w1lambda = w1r - w1;
-    const float w0lambda = 1.f - w1lambda;
+      // Compute X axis lambdas
+      const float w1r = rwidth * out_x;
+      const int w1 = static_cast<int>(w1r);
+      const int w1p = (w1 < input_width - 1) ? 1 : 0;
+      const float w1lambda = w1r - w1;
+      const float w0lambda = 1.f - w1lambda;
 
-    // TODO: check if this is correct
-    // n * chw + c*hw + h*w + w; 
-    int out_idx = n * num_channels * output_height * output_width + c * output_height * output_width + out_y * output_width + out_x;
+      // TODO: check if this is correct
+      // n * chw + c*hw + h*w + w;
+      int out_idx = n * num_channels * output_height * output_width +
+                    c * output_height * output_width + out_y * output_width +
+                    out_x;
 
-    Y[out_idx] =
-        static_cast<scalar_t>(h0lambda *
-             (w0lambda *
-                  __ldg(&X[idx(
-                      n, num_channels, c, input_height, input_width, h1, w1)]) +
-              w1lambda *
-                  __ldg(&X[idx(
-                      n,
-                      num_channels,
-                      c,
-                      input_height,
-                      input_width,
-                      h1,
-                      w1 + w1p)])) +
-         h1lambda *
-             (w0lambda *
-                  __ldg(&X[idx(
-                      n,
-                      num_channels,
-                      c,
-                      input_height,
-                      input_width,
-                      h1 + h1p,
-                      w1)]) +
-              w1lambda *
-                  __ldg(&X[idx(
-                      n,
-                      num_channels,
-                      c,
-                      input_height,
-                      input_width,
-                      h1 + h1p,
-                      w1 + w1p)])));
-
+      Y[out_idx] = static_cast<scalar_t>(
+          h0lambda * (w0lambda * __ldg(&X[idx(n, num_channels, c, input_height,
+                                              input_width, h1, w1)]) +
+                      w1lambda * __ldg(&X[idx(n, num_channels, c, input_height,
+                                              input_width, h1, w1 + w1p)])) +
+          h1lambda *
+              (w0lambda * __ldg(&X[idx(n, num_channels, c, input_height,
+                                       input_width, h1 + h1p, w1)]) +
+               w1lambda * __ldg(&X[idx(n, num_channels, c, input_height,
+                                       input_width, h1 + h1p, w1 + w1p)])));
+    }
   }
-
-  }
-//  }
+  //  }
 }
 
 // input is dY, output is dX
 template <typename scalar_t>
 __global__ void bilinearBackwardKernel(
-    const int input_size,
-    const int num_channels,
-    const int input_height,
-    const int input_width,
-    const int output_height,
-    const int output_width,
-    const scalar_t* const __restrict__ dY,
-    scalar_t* const __restrict__ dX) {
+    const int input_size, const int num_channels, const int input_height,
+    const int input_width, const int output_height, const int output_width,
+    const scalar_t *const __restrict__ dY, scalar_t *const __restrict__ dX) {
 
-    const float height_scale = 1.0f * output_height / input_height;
-    const float width_scale = 1.0f * output_width / input_width;
+  const float height_scale = 1.0f * output_height / input_height;
+  const float width_scale = 1.0f * output_width / input_width;
 
-    for (size_t index = blockDim.x * blockIdx.x + threadIdx.x;
-         index < input_size; index += blockDim.x * gridDim.x) {
+  for (size_t index = blockDim.x * blockIdx.x + threadIdx.x; index < input_size;
+       index += blockDim.x * gridDim.x) {
 
     int indexTemp = index;
     const int in_x = indexTemp % input_width;
@@ -222,47 +185,30 @@ __global__ void bilinearBackwardKernel(
 
     const scalar_t dYi = __ldg(&dY[index]);
 
-    const int out_numel = input_size / (input_height * input_width) * output_height * output_width;
+    const int out_numel = input_size / (input_height * input_width) *
+                          output_height * output_width;
 
-      fastAtomicAdd<scalar_t>(
-          dX, 
-          idx(n, num_channels, c, output_height, output_width, h1, w1),
-          out_numel,
-          static_cast<scalar_t>(h0lambda * w0lambda * dYi)
-      );
+    fastAtomicAdd<scalar_t>(
+        dX, idx(n, num_channels, c, output_height, output_width, h1, w1),
+        out_numel, static_cast<scalar_t>(h0lambda * w0lambda * dYi));
 
-      fastAtomicAdd<scalar_t>(
-        dX,
-        idx(n, num_channels, c, output_height, output_width, h1, w1 + w1p),
-        out_numel,
-        static_cast<scalar_t>(h0lambda * w1lambda * dYi)
-      );
+    fastAtomicAdd<scalar_t>(
+        dX, idx(n, num_channels, c, output_height, output_width, h1, w1 + w1p),
+        out_numel, static_cast<scalar_t>(h0lambda * w1lambda * dYi));
 
-      fastAtomicAdd<scalar_t>(
-        dX,
-        idx(n, num_channels, c, output_height, output_width, h1 + h1p, w1),
-        out_numel,
-        static_cast<scalar_t>(h1lambda * w0lambda * dYi)
-      );
+    fastAtomicAdd<scalar_t>(
+        dX, idx(n, num_channels, c, output_height, output_width, h1 + h1p, w1),
+        out_numel, static_cast<scalar_t>(h1lambda * w0lambda * dYi));
 
-      fastAtomicAdd<scalar_t>(
-        dX,
-        idx(
-            n,
-            num_channels,
-            c,
-            output_height,
-            output_width,
-            h1 + h1p,
-            w1 + w1p),
-        out_numel,
-        static_cast<scalar_t>(h1lambda * w1lambda * dYi)
-      );
-
+    fastAtomicAdd<scalar_t>(dX, idx(n, num_channels, c, output_height,
+                                    output_width, h1 + h1p, w1 + w1p),
+                            out_numel,
+                            static_cast<scalar_t>(h1lambda * w1lambda * dYi));
   }
 }
 
-at::Tensor bilinear_cuda_forward(at::Tensor& in, const int new_h, const int new_w) {
+at::Tensor bilinear_cuda_forward(at::Tensor &in, const int new_h,
+                                 const int new_w) {
 
   const int nIn = in.size(0);
   const int cIn = in.size(1);
@@ -275,30 +221,23 @@ at::Tensor bilinear_cuda_forward(at::Tensor& in, const int new_h, const int new_
   const dim3 block(256);
   const dim3 grid(((outSize / cIn / nIn) + block.x - 1) / block.x);
 
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(in.type(), "bilinearForwardKernel", ([&]
-    {
-     
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+      in.type(), "bilinearForwardKernel", ([&] {
+
         bilinearForwardKernel<scalar_t><<<grid, block>>>(
-                                        out.numel(),
-                                        cIn,
-                                        hIn,
-                                        wIn,
-                                        new_h,
-                                        new_w,
-                                        in.data<scalar_t>(),
-                                        out.data<scalar_t>()
-                                      ); 
+            out.numel(), cIn, hIn, wIn, new_h, new_w, in.data<scalar_t>(),
+            out.data<scalar_t>());
 
-    }));
+      }));
 
-    AT_CHECK(cudaGetLastError() == cudaSuccess,
-          "issue with bilinearForwardKernel, CUDA code ",
-          cudaGetLastError());
+  AT_CHECK(cudaGetLastError() == cudaSuccess,
+           "issue with bilinearForwardKernel, CUDA code ", cudaGetLastError());
 
-  return out; 
+  return out;
 }
 
-at::Tensor bilinear_cuda_backward(at::Tensor& in, const int out_h, const int out_w) {
+at::Tensor bilinear_cuda_backward(at::Tensor &in, const int out_h,
+                                  const int out_w) {
 
   const int nIn = in.size(0);
   const int cIn = in.size(1);
@@ -311,25 +250,17 @@ at::Tensor bilinear_cuda_backward(at::Tensor& in, const int out_h, const int out
   const dim3 block(256);
   const dim3 grid((inSize + block.x - 1) / block.x);
 
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(in.type(), "bilinearBackwardKernel", ([&]
-    {
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(
+      in.type(), "bilinearBackwardKernel", ([&] {
 
         bilinearBackwardKernel<scalar_t><<<grid, block>>>(
-                                        in.numel(),
-                                        cIn,
-                                        hIn,
-                                        wIn,
-                                        out_h,
-                                        out_w,
-                                        in.data<scalar_t>(),
-                                        out.data<scalar_t>()
-                                      ); 
+            in.numel(), cIn, hIn, wIn, out_h, out_w, in.data<scalar_t>(),
+            out.data<scalar_t>());
 
-    }));
+      }));
 
-    AT_CHECK(cudaGetLastError() == cudaSuccess,
-          "issue with bilinearForwardKernel, CUDA code ",
-          cudaGetLastError());
+  AT_CHECK(cudaGetLastError() == cudaSuccess,
+           "issue with bilinearForwardKernel, CUDA code ", cudaGetLastError());
 
-    return out;
+  return out;
 }
---
+++ test2.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn.functional as F
+
+import numpy as np
+
+from matplotlib.image import imread, imsave
+
+from upsampling import Interpolation
+
+fp16 = True
+
+num_runs = 100
+
+log = False
+
+# foo = torch.randn(1, 1, 5, 5, requires_grad=True).cuda()
+
+#x = imread("starry_small.jpg")
+#foo = x.swapaxes(1, 2).swapaxes(0, 1)
+#foo = np.expand_dims(x, axis=0)
+
+# NWHC to NCHW
+#foo = torch.from_numpy(foo).cuda().float().permute(0, 3, 1, 2)
+
+foo = torch.randn(128, 3, 224, 224).cuda()
+
+foo.requires_grad = True
+
+if fp16:
+    foo = foo.half()
+
+for i in range(num_runs):
+    bar = F.interpolate(foo, scale_factor=2, mode='bilinear', align_corners=True) # False
+
+if log:
+    print("Original data")
+    print(foo)
+    print("Reference implementation")
+    print(bar)
+
+interp = Interpolation()
+
+for i in range(num_runs):
+    baz = interp(foo, scale_factor=2)
+
+if log:
+    print("My implementation")
+
+    print(baz)
+    
+    print("Max fprop difference:")
+    
+    print(torch.max(torch.abs(bar - baz)))
+    metric = torch.abs(bar - baz) / torch.abs(bar) * 100
+    print("Mean percentage error: {}".format(torch.mean(metric)))
+    print("Max percentage error: {}".format(torch.max(metric)))
+
+def set_grad(var):
+    def hook(grad):
+        var.grad = grad
+    return hook
+
+bar2 = bar.sum()
+baz2 = baz.sum()
+
+#bar.register_hook(set_grad(bar))
+#baz.register_hook(set_grad(baz))
+#
+#bar2.register_hook(set_grad(bar2))
+#baz2.register_hook(set_grad(baz2))
+
+for i in range(num_runs):
+    bar2.backward(retain_graph=True)
+    baz2.backward(retain_graph=True)
+
+#print("bar2: {}".format(bar2))
+#print("baz2: {}".format(baz2))
+
+#spam = bar.detach().squeeze().permute(1, 2, 0).float().cpu().numpy().astype(np.uint8)
+
+#ham = baz.detach().squeeze().permute(1, 2, 0).float().cpu().numpy().astype(np.uint8)
+
+#print(spam.shape)
+#print(ham.shape)
+
+#print(spam)
+#print(ham)
+#print(spam - ham)
+
+#pic = np.squeeze(spam).swapaxes(0, 1).swapaxes(1, 2).astype(np.uint8)
+#pic = ham
+#print(pic.shape)
+#print(pic.dtype)
+
+#print(x)
+#print("\n\n")
+#print(pic)
+
+#imsave('resized.jpg', pic)
+
+#print(bar.grad, baz.grad)
+#print(bar2.grad, baz2.grad)
+
+
+#print("Maximum backprop difference for [bar, baz]:")
+#print(torch.max(torch.abs(bar.grad - baz.grad)))
+#
+#print("Maximum backprop difference for [bar2, baz2]:")
+#print(torch.max(torch.abs(bar2.grad - baz2.grad)))
+
+# print(bar - baz)
