e3ab7761f558eca1a572862addd1db934d753329
Marek Kolodziej <mkolod@gmail.com>
2019-04-22 08:44:13 -0700

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-setup(name='nv_upsampling',
+setup(name='nv_bilinear_upsampling',
 
       ext_modules=[
-          CUDAExtension('nv_upsampling_cuda', ['nv_upsampling.cpp', 'nv_upsampling_cuda_kernel.cu'],
+          CUDAExtension('nv_bilinear_upsampling_cuda', ['nv_upsampling.cpp', 'nv_upsampling_cuda_kernel.cu'],
 
       extra_compile_args={
         'cxx':  ['-std=c++14', '-O3', '-Wall'],
@@ -18,36 +18,6 @@ setup(name='nv_upsampling',
             '--use_fast_math'
             ]
         })],
-
+      version='0.1.0',
+      py_modules=['bilinear_upsampling'],
       cmdclass={'build_ext': BuildExtension})
-
-#          CppExtension('nv_upsampling_cpu', ['nv_upsampling.cpp']),
-
-#from setuptools import setup
-#from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-#
-#package = 'adlr-interpolation'
-#interpolation_module = CUDAExtension('interpolation_cuda', [
-#    './src/interpolation.cpp',
-#    './src/interpolation_kernels.cu'
-#    ],
-#    extra_compile_args={
-#        'cxx':  ['-Wall', '-std=c++14'],
-#        'nvcc': [
-#            '-gencode',   'arch=compute_61,code=compute_61',
-#            '-gencode',   'arch=compute_70,code=sm_70',
-#            '-Xcompiler', '-Wall',
-#            '-std=c++14'
-#            ]
-#        }
-#    )
-#
-#setup(name=package,
-#    version='0.1.0.0',
-#    author="Alexey Kamenev",
-#    description="Native (CUDA) implementation of interpolation",
-#    py_modules=['interpolation'],
-#    ext_modules=[interpolation_module],
-#    cmdclass={'build_ext': BuildExtension})
-#
-#
