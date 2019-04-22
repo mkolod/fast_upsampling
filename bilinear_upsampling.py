cab04abc4b7f937a4381a6579ecfb8414113aeec
Marek Kolodziej <mkolod@gmail.com>
2019-04-22 09:17:47 -0700

    Package name change

--- bilinear_upsampling.py
+++ bilinear_upsampling.py
@@ -1,5 +1,5 @@
 import torch
-import nv_upsampling_cuda
+import nv_bilinear_upsampling_cuda as ups_cuda
 
 class InterpolationFunction(torch.autograd.Function):
     @staticmethod
@@ -8,14 +8,14 @@ class InterpolationFunction(torch.autograd.Function):
         assert dims or scale_factor, "either new dims (new_h, new_w) or scale factor need to be given"
         if scale_factor is not None:
             new_h, new_w = int(input.shape[-2] * scale_factor), int(input.shape[-1] * scale_factor)
-        outputs = nv_upsampling_cuda.bilinear_forward(input.contiguous(), new_h, new_w)
+        outputs = ups_cuda.bilinear_forward(input.contiguous(), new_h, new_w)
         variables = [torch.as_tensor(input.size(-2)), torch.as_tensor(input.size(-1))]
         ctx.save_for_backward(*variables)
         return outputs
 
     @staticmethod
     def backward(ctx, grad_o):
-        outputs = nv_upsampling_cuda.bilinear_backward(grad_o.contiguous(), *ctx.saved_variables)
+        outputs = ups_cuda.bilinear_backward(grad_o.contiguous(), *ctx.saved_variables)
         return outputs, None, None, None
 
 class Interpolation(torch.nn.Module):
